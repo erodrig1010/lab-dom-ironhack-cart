1fc7cdbe650a4d338120bddb8325cf5b5e1f2b83
Eleyni Rodriguez <eleyni.m.rodriguez@gmail.com>
2018-04-12 09:37:07 -0400

    Create new items functionality added. WIP calculating product total for new items.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -5,14 +5,17 @@ function deleteItem(e){
 }
 
 
-function getPriceByProduct(){
 
-}
+// function getPriceByProduct(){
+
+// }
 
 
-function updatePriceByProduct(productPrice, index){
 
-}
+// function updatePriceByProduct(productPrice, index){
+
+// }
+
 
 
 function getTotalPrice() {
@@ -34,7 +37,7 @@ for (var i=0; i < unitQuantity.length; i++) {
   unitQuantityArray.push(unitQuantityValue);
   // console.log(unitQuantityArray);
   if (unitQuantityValue === "") {
-    unitQuantityValue = "0";
+    unitQuantityValue = "0.00";
   }
 }
 
@@ -67,34 +70,138 @@ for (var i=0; i < finalTotal.length; i++) {
 }
 
 
-function createQuantityInput(){
-
-}
-
 
 function createDeleteButton(){
+  var buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute("class", "col-lg-2");
+  var newButton = document.createElement("button");
+  newButton.className = "btn btn-delete";
+  newButton.innerText = "Delete";
+  newButton.onclick = deleteItem;
+  buttonDiv.appendChild(newButton);
 
+  return buttonDiv;
 }
 
 
-function createQuantityNode(){
 
+function createNameNode(newName){
+  var nameDiv = document.createElement("div");
+  nameDiv.setAttribute("class", "col-lg-2");
+  var nameSpan = document.createElement("span");
+  nameSpan.setAttribute("class", "productName")
+  nameSpan.innerHTML = newName;
+  nameDiv.appendChild(nameSpan);
+  console.log(nameDiv);
+
+  return nameDiv
 }
 
 
-function createItemNode(dataType, itemData){
 
+function createPriceNode(newPrice){
+  var priceDiv = document.createElement("div");
+  priceDiv.setAttribute("class", "col-lg-2")
+  var priceSpan = document.createElement("span");
+  priceSpan.setAttribute("class", "costPerUnit")
+  priceSpan.innerHTML = "$" + newPrice;
+  priceDiv.appendChild(priceSpan);
+  console.log(priceDiv);
+
+  return priceDiv
 }
 
 
-function createNewItemRow(itemName, itemUnitPrice){
 
+function createQuantityInput(){
+  var newQtyDiv = document.createElement("div");
+  newQtyDiv.setAttribute("class", "col-lg-4");
+  newQtyDiv.innerHTML = "QTY";
+  var newQtyInput = document.createElement("input");
+  newQtyInput.setAttribute("class", "itemQuantity");
+  newQtyDiv.appendChild(newQtyInput);
+  console.log(newQtyDiv);
+
+  return newQtyDiv
+}
+
+
+
+function createItemTotalNode(){
+  var itemTotalPrice = getTotalPrice();
+  var itemTotalPriceDiv = document.createElement("div");
+  itemTotalPriceDiv.setAttribute("class", "col-lg-2")
+  var itemTotalPriceSpan = document.createElement("span");
+  itemTotalPriceSpan.setAttribute("class", "totalPrice");
+  itemTotalPriceSpan.innerHTML = itemTotalPrice;
+  itemTotalPriceDiv.appendChild(itemTotalPriceSpan);
+  console.log(itemTotalPriceDiv);
+
+  return itemTotalPriceDiv
+}
+
+
+
+function createNewItemRow(newName, newPrice){
+  var getValueOfProductName = document.getElementsByClassName("newName").value;
+  // console.log(getValueOfProductName);
+  var getValueOfProductCost = document.getElementsByClassName("newPrice").value;
+  var itemRow = document.createElement("div");
+  itemRow.setAttribute("class", "item row");
+  var productName = createNameNode(newName);
+  var productPrice = createPriceNode(newPrice);
+  var qtyInput = createQuantityInput();
+  var itemTotalPrice = createItemTotalNode();
+  var deleteBtn = createDeleteButton();
+  itemRow.appendChild(productName);
+  itemRow.appendChild(productPrice);
+  itemRow.appendChild(qtyInput);
+  itemRow.appendChild(itemTotalPrice);
+  itemRow.appendChild(deleteBtn);
+  // console.log(itemRow);
+
+  return itemRow;
 }
 
 
 function createNewItem(){
-
+  var newProductName = document.getElementById("newName").value;
+  // console.log(newProductName);
+  var newProductPrice = document.getElementById("newPrice").value;
+  // console.log(newProductPrice);
+  var newItem = createNewItemRow(newProductName, newProductPrice);
+  var newDiv = document.getElementById("newItemLine");
+  newDiv.parentNode.insertBefore(newItem, newDiv);
+  document.getElementById("newName").value = "";
+  document.getElementById("newPrice").value = "";
 }
+
+
+// function newItemHtml () {
+//   return 
+//   "<div class="container">" + 
+//   "<div class="item row">" + 
+//     "<div class="col-lg-2">" +
+//       "<span class="productName">"+itemName+"</span>" +
+//     "</div>" +
+//     "<div class="col-lg-2">" + 
+//         "$" + "<span class="costPerUnit">" + itemUnitPrice + "</span>" +
+//     "</div>" +
+//     "<div class="col-lg-4">" +
+//       "QTY" + "<input class="itemQuantity" type="text" name="quantity">" +
+//     "</div>" +
+//     "<div class="col-lg-2">" +
+//       "$" + "<span class="totalPrice">" + totalItemPriceValues + "</span>" +
+//     "</div>" +
+//     "<div class="col-lg-2">" +
+//       "<button type="button" class="btn btn-delete">Delete</button>" +
+//     "</div>" +
+//   "</div>" +
+//   "</div>"
+// }
+
+
+
 
 
 window.onload = function(){
@@ -105,9 +212,7 @@ window.onload = function(){
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  for(var i = 0; i<deleteButtons.length; i++){
+    deleteButtons[i].onclick = deleteItem
   }
 };
-
-
